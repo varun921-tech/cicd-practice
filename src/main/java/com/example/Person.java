b2a8cc2341e529ff476c327660ffbcabc24adee3
Varun Garg <varun_garg@epam.comsdfsdfs>
2025-04-25 11:29:38 +0530

    Added bad code in Person.java

--- src/main/java/com/example/Person.java
+++ src/main/java/com/example/Person.java
@@ -1,21 +1,25 @@
 package com.example;
 
 public class Person {
-    private String name;
-    private int age;
+  private String name;
+  private int age;
 
-    public Person(String name, int age){
-        this.name = name;
-        this.age = age;
+  public Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public static void main(String[] args) {
+    if (true) {
     }
-
-    public String getName(){
-        return this.name;
-    }
-
-    public int getAge(){
-        return this.age;
-    }
-
+  }
 
 }
